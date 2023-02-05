fbcf7eefa4d938b254bb026e5e613af2bd3fded7
Elena Nikiforova <vizualnoe@gmail.com>
2023-02-05 22:20:02 +0100

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({})],
   controllers: [AppController],
   providers: [],
 })
